377f4596f387fc0e4b93fe212f9e6aba2762c8c2
Bhakti <bhaktipatil403@gmail.com>
2023-07-18 20:46:45 +0530

    uplaoding on github

---
+++ about.php
@@ -0,0 +1,219 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>about</title>
+    <!--swiper css link-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css"/>
+    <!--font awesome coding link-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <!--custom css file link-->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <!--header section starts-->
+   <section class="header">
+    <a href="home.php" class="logo">travel.</a>
+    <nav class="navbar">
+        <a href="home.php">home</a>
+        <a href="about.php">about</a>
+        <a href="package.php">package</a>
+        <a href="book.php">book</a>
+    </nav>
+    <div id="menu-btn" class="fas fa-bars"></div>
+   </section>
+
+  <!--header section ends-->
+
+<div class="heading" style="background:url(images/img7.jpg) no-repeat">
+    <h1>about us</h1>
+</div>
+
+<!--about section starts-->
+
+<section class="about">
+
+   <div class="image">
+    <img src="images/explore.jpg" alt="">
+   </div>
+
+   <div class="content">
+    <h3>why choose us?</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias dolorem minima cupiditate sequi, quis nobis temporibus consequuntur exercitationem nesciunt perspiciatis omnis. Quas impedit voluptatem dolores debitis? Reiciendis debitis dolor laudantium!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo nulla asperiores tempore sapiente explicabo dolor maxime amet eius voluptatum dolorum?</p>
+    <div class="icons-container">
+        <div class="icons">
+            <i class="fas fa-map"></i>
+            <span>top destinations</span>
+        </div>
+        <div class="icons">
+            <i class="fas fa-hand-holding-usd"></i>
+            <span>affordable price</span>
+        </div>
+        <div class="icons">
+            <i class="fas fa-headset"></i>
+            <span>24/7 guide service</span>
+        </div>
+    </div>
+   </div>
+
+</section>
+
+<!--about section ends-->
+
+<!--reviews section starts-->
+
+<section class="reviews">
+<div style="display:grid;row-gap:20px;">
+<div class="swiper-wrapper" style="column-gap:50px">
+
+    <!--<div>sandip</div>
+    <div>bhakti</div>
+   <div>dipak</div>-->
+    <div class="swiper-slider slide">
+        <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+        </div>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima qui dicta sit, nostrum dignissimos labore nulla eum, excepturi esse accusantium repellat vel dolore ducimus, quidem numquam placeat iste cumque? Rem.</p>
+        <h3>john deo</h3>
+        <span>traveler</span>
+        <img src="images/pic1.jfif" alt="">
+    </div>
+
+    <div class="swiper-slider slide">
+        <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+        </div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus ipsa corrupti aliquam nam, repellat culpa. Suscipit voluptate corporis facilis sequi, harum iusto deserunt sed animi quidem delectus accusamus. Tempora dolores dolorem fugit similique. Praesentium, architecto.</p>
+        <h3>john deo</h3>
+        <span>traveler</span>
+        <img src="images/pic2.jpg" alt="">
+    </div>
+
+    <div class="swiper-slider slide">
+        <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+        </div>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima qui dicta sit, nostrum dignissimos labore nulla eum, excepturi esse accusantium repellat vel dolore ducimus, quidem numquam placeat iste cumque? Rem.</p>
+        <h3>john deo</h3>
+        <span>traveler</span>
+        <img src="images/pic3.jpg" alt="">
+    </div>
+
+  </div>
+
+  <div class="swiper-wrapper" style="column-gap:50px">
+  <div class="swiper-slider slide">
+        <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+        </div>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima qui dicta sit, nostrum dignissimos labore nulla eum, excepturi esse accusantium repellat vel dolore ducimus, quidem numquam placeat iste cumque? Rem.</p>
+        <h3>john deo</h3>
+        <span>traveler</span>
+        <img src="images/pic4.jpg" alt="">
+    </div>
+
+    <div class="swiper-slider slide">
+        <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+        </div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus ipsa corrupti aliquam nam, repellat culpa. Suscipit voluptate corporis facilis sequi, harum iusto deserunt sed animi quidem delectus accusamus. Tempora dolores dolorem fugit similique. Praesentium, architecto.</p>
+        <h3>john deo</h3>
+        <span>traveler</span>
+        <img src="images/pic5.jfif" alt="">
+    </div>
+
+    <div class="swiper-slider slide">
+        <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+        </div>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima qui dicta sit, nostrum dignissimos labore nulla eum, excepturi esse accusantium repellat vel dolore ducimus, quidem numquam placeat iste cumque? Rem.</p>
+        <h3>john deo</h3>
+        <span>traveler</span>
+        <img src="images/pic6.jpg" alt="">
+    </div>
+  </div>
+</div>
+
+</section>
+
+<!--reviews section ends-->
+
+
+
+
+
+
+
+
+
+
+
+<!--footer section starts-->
+ <section class="footer">
+    <div class="box-container">
+        <div class="box">
+            <h3>quick links</h3>
+        <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+        <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+        <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+        <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+        </div>
+
+        <div class="box">
+            <h3>extra links</h3>
+        <a href="#"> <i class="fas fa-angle-right"></i>ask questions</a>
+        <a href="#"> <i class="fas fa-angle-right"></i>about us</a>
+        <a href="#"> <i class="fas fa-angle-right"></i>privacy and policy</a>
+        <a href="#"> <i class="fas fa-angle-right"></i>terms of use</a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+        <a href="#"> <i class="fas fa-phone"></i> +123-456-7890 </a>
+        <a href="#"> <i class="fas fa-phone"></i> +111-222-3333 </a>
+        <a href="#"> <i class="fas fa-envelope"></i> bhaktipatil@gmail.com </a>
+        <a href="#"> <i class="fas fa-phone"></i> Nanded, India - 431606 </a>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+            <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+            <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+            <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+        </div>
+
+    </div>
+    <div class="credit"> created by <span> mr.web.designer </span> | all rights reserved! </div>
+ </section>
+<!--footer section ends-->
+
+<!--swiper js link-->
+<script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+<!--custom js file link-->
+<script src="js/script.js"></script>
+
+</body>
+</html>
